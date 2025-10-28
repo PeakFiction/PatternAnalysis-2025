4660466303b1514121daca1b458d460ea28b9542
s4906358 <s4906358@login0.compute.eait.uq.edu.au>
2025-10-28 14:15:59 +1000

    Updated Model

--- recognition/HipMRIProject/modules.py
+++ recognition/HipMRIProject/modules.py
@@ -3,19 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
+    """(convolution => [InstanceNorm] => LeakyReLU) * 2"""
 
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
+            # Replaced BatchNorm2d with InstanceNorm2d
+            # Replaced ReLU with LeakyReLU
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            nn.InstanceNorm2d(mid_channels),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True)
         )
 
     def forward(self, x):
@@ -99,3 +101,4 @@ class UNet(nn.Module):
         x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
+
